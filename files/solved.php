375dea6c53b9e7ed1edc9e0c902aec5f924492a4
Guillaume Kulakowski <guillaume@llaumgui.com>
2011-09-05 20:32:51 +0200

    version 1.0.1

--- files/solved.php
+++ files/solved.php
@@ -3,7 +3,7 @@
 /**
  * File call for mark topic like solved.
  *
- * @version 1.0
+ * @version 1.0.1
  * @package FluxSolved
  * @copyright Copyright (c) 2010 Guillaume Kulakowski and contributors
  * @license http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License v2.0
