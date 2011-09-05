7b5380bc86100816b23a90d421aa2901654aad6c
Guillaume Kulakowski <guillaume@llaumgui.com>
2011-09-05 20:32:03 +0200

    Fix an bd escape issue

--- files/solved.php
+++ files/solved.php
@@ -24,9 +24,7 @@ function solve_topic($topic_id)
 	else
 		$subject = $lang_solved['__solved__'].$subject;
 
-	$db->query('UPDATE '.$db->prefix.'topics SET subject="'.$subject.'" WHERE id='.$topic_id) or error('Unable to mark solved topic', __FILE__, __LINE__, $db->error());
-
-
+	$db->query('UPDATE '.$db->prefix.'topics SET subject="'.$db->escape($subject).'" WHERE id='.$topic_id) or error('Unable to mark solved topic', __FILE__, __LINE__, $db->error());
 }
 
 
@@ -43,7 +41,7 @@ if ($tid < 1)
 	message($lang_common['Bad request']);
 
 // Fetch some info about the post, the topic and the forum
-$result = $db->query('SELECT f.id AS fid, f.forum_name, f.moderators, f.redirect_url, fp.post_replies, fp.post_topics, t.id AS tid, t.subject, t.first_post_id, t.closed, p.posted, p.poster, p.poster_id, p.message, p.hide_smilies FROM '.$db->prefix.'posts AS p INNER JOIN '.$db->prefix.'topics AS t ON t.id=p.topic_id INNER JOIN '.$db->prefix.'forums AS f ON f.id=t.forum_id LEFT JOIN '.$db->prefix.'forum_perms AS fp ON (fp.forum_id=f.id AND fp.group_id='.$pun_user['g_id'].') WHERE (fp.read_forum IS NULL OR fp.read_forum=1) AND t.id='.$tid) or error('Unable to fetch topic info', __FILE__, __LINE__, $db->error());
+$result = $db->query('SELECT f.id AS fid, f.forum_name, f.moderators, f.redirect_url, fp.post_replies, fp.post_topics, t.id AS tid, t.subject, t.first_post_id, t.closed, p.posted, p.poster, p.poster_id, p.message, p.hide_smilies FROM '.$db->prefix.'posts AS p INNER JOIN '.$db->prefix.'topics AS t ON t.id=p.topic_id INNER JOIN '.$db->prefix.'forums AS f ON f.id=t.forum_id LEFT JOIN '.$db->prefix.'forum_perms AS fp ON (fp.forum_id=f.id AND fp.group_id='.$pun_user['g_id'].') WHERE (fp.read_forum IS NULL OR fp.read_forum=1) AND t.id='.$tid.' ORDER BY p.id') or error('Unable to fetch topic info', __FILE__, __LINE__, $db->error());
 if (!$db->num_rows($result))
 	message($lang_common['Bad request']);
 
